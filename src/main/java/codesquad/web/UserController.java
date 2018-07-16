8cda5d0fbd1a38b49fb9eeaa3695a8953f0d2ace
우아테크캠프_15 <woowatechcamp_15@woowatechcamp15.local>
2018-07-16 16:39:55 +0900

    Team7_pair1 ATDD 1단계 구현

--- src/main/java/codesquad/web/UserController.java
+++ src/main/java/codesquad/web/UserController.java
@@ -1,6 +1,8 @@
 package codesquad.web;
 
+import codesquad.UnAuthenticationException;
 import codesquad.domain.User;
+import codesquad.security.HttpSessionUtils;
 import codesquad.security.LoginUser;
 import codesquad.service.UserService;
 import org.slf4j.Logger;
@@ -10,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -39,6 +42,17 @@ public class UserController {
         return "/user/list";
     }
 
+    @PostMapping("/login")
+    public String login(String userId, String password, HttpSession httpSession) {
+        // TODO 로그인 기능 구현 및 세션에 User 정보 저장
+        try {
+            httpSession.setAttribute(HttpSessionUtils.USER_SESSION_KEY, userService.login(userId, password));
+            return "redirect:/users";
+        } catch (UnAuthenticationException e) {
+            return "/user/login_failed";
+        }
+    }
+
     @GetMapping("/{id}/form")
     public String updateForm(@LoginUser User loginUser, @PathVariable long id, Model model) {
         model.addAttribute("user", userService.findById(loginUser, id));
@@ -50,5 +64,4 @@ public class UserController {
         userService.update(loginUser, id, target);
         return "redirect:/users";
     }
-
 }
--- src/test/java/codesquad/web/UserAcceptanceTest.java
+++ src/test/java/codesquad/web/UserAcceptanceTest.java
@@ -2,17 +2,15 @@ package codesquad.web;
 
 import codesquad.domain.User;
 import codesquad.domain.UserRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import support.test.AcceptanceTest;
-
-import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,6 +19,12 @@ public class UserAcceptanceTest extends AcceptanceTest {
 
     @Autowired
     private UserRepository userRepository;
+    private User user;
+
+    @Before
+    public void setUp() throws Exception {
+        user = new User(3, "testuser", "password", "자바지기", "javajigi@slipp.net");
+    }
 
     @Test
     public void createForm() throws Exception {
@@ -31,22 +35,15 @@ public class UserAcceptanceTest extends AcceptanceTest {
 
     @Test
     public void create() throws Exception {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        String userId = "testuser";
-        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
-        params.add("userId", userId);
-        params.add("password", "password");
-        params.add("name", "자바지기");
-        params.add("email", "javajigi@slipp.net");
-        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
-
-        ResponseEntity<String> response = template().postForEntity("/users", request, String.class);
+        ResponseEntity<String> response = template().postForEntity("/users", HtmlFormDataBuilder.urlEncodedForm()
+                .addParameter("userId", user.getUserId())
+                .addParameter("password", user.getPassword())
+                .addParameter("name", user.getName())
+                .addParameter("email", user.getEmail())
+                .build(), String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FOUND);
-        assertThat(userRepository.findByUserId(userId).isPresent()).isTrue();
+        assertThat(userRepository.findByUserId(user.getUserId()).isPresent()).isTrue();
         assertThat(response.getHeaders().getLocation().getPath()).startsWith("/users");
     }
 
@@ -81,16 +78,12 @@ public class UserAcceptanceTest extends AcceptanceTest {
     }
 
     private ResponseEntity<String> update(TestRestTemplate template) throws Exception {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
-        params.add("_method", "put");
-        params.add("password", "test");
-        params.add("name", "자바지기2");
-        params.add("email", "javajigi@slipp.net");
-        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
+        HttpEntity<MultiValueMap<String, Object>> request = HtmlFormDataBuilder.urlEncodedForm()
+                .addParameter("_method", "put")
+                .addParameter("password", "test")
+                .addParameter("name", "자바지기2")
+                .addParameter("email", "javajigi@slipp.net")
+                .build();
 
         return template.postForEntity(String.format("/users/%d", defaultUser().getId()), request, String.class);
     }
